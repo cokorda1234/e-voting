caf1dfd27922967f2d3ae4d122ebf400e5d4d022
cokorda1234 <cokordagung1@gmail.com>
2021-01-05 20:40:55 +0800

    fix

--- database/migrations/2021_01_05_123920_votes.php
+++ database/migrations/2021_01_05_123920_votes.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Vote extends Migration
+class Votes extends Migration
 {
     /**
      * Run the migrations.
